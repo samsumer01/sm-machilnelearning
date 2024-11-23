e395478a4be6229ba6dff37ac7c8324f240ff795
sam <145283692+samsumer01@users.noreply.github.com>
2024-11-23 10:24:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,16 @@ with st.expander('Data'):
  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
  df
 
-  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
+#   st.write('**X**')
+#   X_raw = df.drop('species', axis=1)
+#   X_raw
 
-  st.write('**y**')
-  y_raw = df.species
-  y_raw
+#   st.write('**y**')
+#   y_raw = df.species
+#   y_raw
 
-with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+# with st.expander('Data visualization'):
+#   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 # Input features
 # with st.sidebar:
