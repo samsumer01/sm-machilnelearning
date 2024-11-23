fe5090e97627b418846ce43135f0cfc392da995e
sam <145283692+samsumer01@users.noreply.github.com>
2024-11-23 10:39:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,49 +12,7 @@ st.info('This is app builds a machine learning model!')
 df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 dp
 
-
-#   st.write('**X**')
-#   X_raw = df.drop('species', axis=1)
-#   X_raw
-
-#   st.write('**y**')
-#   y_raw = df.species
-#   y_raw
-
-# with st.expander('Data visualization'):
-#   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-
-# Input features
-# with st.sidebar:
-#   st.header('Input features')
-#   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-#   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-#   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-#   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-#   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-#   gender = st.selectbox('Gender', ('male', 'female'))
-  
-#   # Create a DataFrame for the input features
-#   data = {'island': island,
-#           'bill_length_mm': bill_length_mm,
-#           'bill_depth_mm': bill_depth_mm,
-#           'flipper_length_mm': flipper_length_mm,
-#           'body_mass_g': body_mass_g,
-#           'sex': gender}
-#   input_df = pd.DataFrame(data, index=[0])
-#   input_penguins = pd.concat([input_df, X_raw], axis=0)
-
-# with st.expander('Input features'):
-#   st.write('**Input penguin**')
-#   input_df
-#   st.write('**Combined penguins data**')
-#   input_penguins
-
-
-# # Data preparation
-# # Encode X
-# encode = ['island', 'sex']
-# df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+# s(input_penguins, prefix=encode)
 
 # X = df_penguins[1:]
 # input_row = df_penguins[:1]
